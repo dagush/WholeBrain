8c21a506b7f0bb2f9450e65dd6fc845bfe27d320
dagush <dagushh@gmail.com>
2019-06-12 17:58:37 +0200

    moved to funcitons folder, plus more and more refactoring...

--- GetBalancedWeights.py
+++ GetBalancedWeights.py
@@ -4,31 +4,17 @@
 # for varying global couplings (G)
 # Saves the steady states and the feedback inhibition (J).
 #
-#
-# For an isolated node, an input to the excitatory pool equal to I_i^E - b_E/a_E = -0.026; 
-# i.e., slightly inhibitory dominated, leads to a firing rate equal to 3.0631 Hz. 
-# Hence, in the large-scale model of interconnected brain areas, 
-# we aim to constraint in each brain area (i) the local feedback inhibitory weight Ji such 
-# that I_i^E - b_E/a_E = -0.026 is fulfilled (with a tolerance of +-0.005). 
-# To achieve this, we apply following procedure: we simulate during 5000 steps 
-# the system of stochastic differential DMF Equations and compute the averaged level of 
-# the input to the local excitatory pool of each brain area,
-# then we upregulate the corresponding local feedback inhibition J_i = J_i + delta;
-# otherwise, we downregulate J_i = J_i - delta. 
-# We recursively repeat this procedure until the constraint on the input
-# to the local excitatory pool is fulfilled in all N brain areas.
-#
 # see:
 # Deco et al. (2014) J Neurosci.
 # http://www.jneurosci.org/content/34/23/7886.long
 #
-# Adrian Ponce-Alvarez
+# Adrian Ponce-Alvarez. Refactoring by Gustavo Patow
 #--------------------------------------------------------------------------
 
 import numpy as np
 import scipy.io as sio
-#from randn2 import randn2
-import Balance_J9
+from functions import Balance_J9
+from functions import DynamicMeanField as DMF
 
 np.random.seed(42)  # Fix the seed for debug purposes...
 
@@ -52,16 +38,17 @@ numW = wes.size  # length(wes);
 # ==========================
 # Some monitoring info: initialization
 N = C.shape[0]
-Balance_J9.Se_init = np.zeros((N, numW))
-Balance_J9.Si_init = np.zeros((N, numW))
-Balance_J9.JI = np.zeros((N, numW))
-
+JI=np.zeros((N,numW))
+Se_init = np.zeros((N,numW))
+Si_init = np.zeros((N,numW))
 for kk, we in enumerate(wes):  # iterate over the weight range (G in the paper, we here)
-    Balance_J9.kk = kk  # Just for monitoring
     J = Balance_J9.JOptim(we, C)
+    Se_init[:, kk] = DMF.sn[:, 0]  # Store steady states S^E (after many iterations/simulations)
+    Si_init[:, kk] = DMF.sg[:, 0]  # Store steady states S^I
+    JI[:,kk]=J[:,0]
 
-sio.savemat('BenjiBalancedWeights.mat',
+sio.savemat('BenjiBalancedWeights-test.mat', #{'JI': JI})
             {'wes': wes,
-             'JI': Balance_J9.JI,
-             'Se_init': Balance_J9.Se_init,
-             'Si_init': Balance_J9.Si_init})  # save Benji_Balanced_weights wes JI Se_init Si_init
+             'JI': JI,
+             'Se_init': Se_init,
+             'Si_init': Si_init})  # save Benji_Balanced_weights wes JI Se_init Si_init
